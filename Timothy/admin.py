0057cf26fd1367f02d8ebb8119cdc55e8f395cfe
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-03-12 19:30:36 +0100

    admin updated

--- Timothy/admin.py
+++ Timothy/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Home, About, Profile, Category, Skills, Languages
 
 # Register your models here.
+
+#HOME
+
+admin.site.register(Home)
+
+
+#ABOUT
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 1
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline]
+
+
+#SKILLS
+
+class SkillsInline(admin.TabularInline):
+    model = Skills
+    extra = 2
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [SkillsInline]
+
+
+#LANGUAGES
+admin.site.register(Languages)  
+
+# Compare this snippet from portfolio/urls.py:
