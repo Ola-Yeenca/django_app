c2e1d9d45b4886f4a20874e520cf2accb6543982
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-03-14 00:22:29 +0100

    building page

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,15 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICIFILES_DIRS = [
     BASE_DIR, 'static',
-    os.path.join(BASE_DIR, 'Timothy/static/assets')
+    os.path.join(BASE_DIR, 'assets')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
