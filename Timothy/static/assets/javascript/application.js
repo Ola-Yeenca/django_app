2f60586489dd3df21e2062b1da9c60a3dce3a03b
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-03-24 00:26:00 +0100

    styliung almost done

--- Timothy/static/assets/javascript/application.js
+++ Timothy/static/assets/javascript/application.js
@@ -1,3 +1,39 @@
-// connect js //
+document.addEventListener("DOMContentLoaded", function() {
+  function useTypewriter(words, loop=true, delaySpeed=500) {
+    const typewriter = document.querySelector('#typewriter');
+    let text = '';
+    let count = 0;
+    let isDeleting = false;
 
-console.log('hello, world');
+    setInterval(() => {
+      const currentWord = words[count];
+      const currentText = text.substring(0, text.length - isDeleting);
+      const currentLength = currentText.length;
+
+      if (!isDeleting && currentLength === currentWord.length) {
+        isDeleting = true;
+      } else if (isDeleting && currentLength === 0) {
+        count = (count + 1) % words.length;
+        isDeleting = false;
+      }
+
+      text = isDeleting
+        ? currentWord.substring(0, currentLength - 1)
+        : currentWord.substring(0, currentLength + 1);
+
+      typewriter.innerHTML = text;
+      if (isDeleting || currentLength < currentWord.length) {
+        typewriter.classList.remove('visible');
+      } else {
+        typewriter.classList.add('visible');
+      }
+    }, isDeleting ? delaySpeed / 2 : delaySpeed);
+  }
+
+  useTypewriter([
+    'Hello, I am Ola Yinka ',
+    'Developer',
+    'Designer',
+    'Freelancer'
+  ]);
+});
